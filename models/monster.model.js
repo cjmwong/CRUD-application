119996d750f8229b2427e96919cc3959d74667c7
cjmwong <cjmwong@ualberta.ca>
2024-02-07 20:16:10 -0700

    initial commit with most files set up

---
+++ models/monster.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const MonsterSchema = new mongoose.Schema(
+    {
+        "id": {type: Number, required: true},
+        "name": {type: String, required: true},
+        "username": {type: String, required: true},
+        "email": {type: String, required: true},
+        "address": {
+            "street": {type: String, required: true},
+            "suite": {type: String, required: true},
+            "city": {type: String, required: true},
+            "zipcode": {type: String, required: true},
+            "geo": {
+                "lat": {type: String, required: true},
+                "lng": {type: String, required: true},
+                }
+            },
+        "phone": {type: String, required: true},
+        "website": {type: String, required: true},
+        "company": {
+            "name": {type: String, required: true},
+            "catchPhrase": {type: String, required: true},
+            "bs": {type: String, required: true},
+            },
+        "image_url": {type: String, required: true},
+    },
+)
+
+export const Monster = mongoose.model("Monster", MonsterSchema)
