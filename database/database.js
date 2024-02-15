edfed0b8e35e6a5b7111aa76e075d58288f01915
cjmwong <cjmwong@ualberta.ca>
2024-02-14 22:24:20 -0700

    changed connection string of db

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
-    const url = `mongodb://localhost:27017/monsters`
+    const url = `process.env.DATABASE_URL ||'mongodb://localhost:27017/monsters`
     try {
         const connection = await mongoose.connect(url);
         console.log("Database connected successfully");
